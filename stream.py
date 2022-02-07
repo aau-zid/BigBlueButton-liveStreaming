aau-zid/BigBlueButton-liveStreaming

commit 3d333687b6a20a8d3f09ef6acd786e96b305123f

custom message in chat on recording

stream.py
@@ -148,6 +148,7 @@ def bbb_browser():
         # ensure chat is enabled (might be locked by moderator)
         if element.is_enabled() and chat_send.is_enabled():
            tmp_chatMsg = os.environ.get('BBB_CHAT_MESSAGE', "This meeting is streamed to")
+           tmp_chatCustomMsg = os.environ.get('BBB_CHAT_CUSTOM_MESSAGE', "This meeting is streamed to")
            if not tmp_chatMsg in [ 'false', 'False', 'FALSE' ]:
                if args.target is not None:
                    tmp_chatUrl = args.target.partition('//')[2].partition('/')[0]
@@ -155,11 +156,13 @@ def bbb_browser():
                        tmp_chatUrl = args.chatUrl
                    if args.chatMsg:
                        tmp_chatMsg = ' '.join(args.chatMsg).strip('"')
-                   element.send_keys("{0}: {1}".format(tmp_chatMsg, tmp_chatUrl))
+                   tmp_chatMsg = "{0}: {1}".format(tmp_chatMsg, tmp_chatUrl)
+               else if tmp_chatCustomMsg != '':
+                   tmp_chatMsg = tmp_chatCustomMsg
                else:
                    tmp_chatMsg = "Recording in progress!"
-                   element.send_keys(tmp_chatMsg)
                
+               element.send_keys(tmp_chatMsg)
                chat_send.click()
 
         if args.chat:

commit 21fb868a84bd895ed8be74ee72fd3fbe60cde27b

stream.py: fix syntax error
Seems to be introduced by 3d333687b6a20a8d3f09ef6acd786e96b305123f and
just merged in 60ca05084001c00a66349febf0e113e82cc6bd09.

stream.py
@@ -156,7 +156,7 @@ def bbb_browser():
                    if args.chatMsg:
                        tmp_chatMsg = ' '.join(args.chatMsg).strip('"')
                    tmp_chatMsg = "{0}: {1}".format(tmp_chatMsg, tmp_chatUrl)
-               else if tmp_chatCustomMsg != '':
+               elif tmp_chatCustomMsg != '':
                    tmp_chatMsg = tmp_chatCustomMsg
                else:
                    tmp_chatMsg = "Recording in progress!"